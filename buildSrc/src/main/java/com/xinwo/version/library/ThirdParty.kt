222a62b9af08cbd9ca46da7c2d6d48ccb15cdfcb
zgj <1032689332@qq.com>
2023-06-27 19:05:08 +0800

    修改依赖格式
    Signed-off-by: zgj <1032689332@qq.com>

--- buildSrc/src/main/java/com/xinwo/version/library/ThirdParty.kt
+++ buildSrc/src/main/java/com/xinwo/version/library/ThirdParty.kt
@@ -1,23 +1,30 @@
 package com.xinwo.version.library
 
 class ThirdParty {
-        val gson = "com.google.code.gson:gson:2.8.2"
+    val gson = "com.google.code.gson:gson:2.8.2"
 
-        val aspectjrt = "org.aspectj:aspectjrt:1.8.11"
+    val aspectjrt = "org.aspectj:aspectjrt:1.8.11"
 
-        val kotlin_version = "1.3.41"
-        val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:" + kotlin_version
+    val kotlin_version = "1.3.41"
+    val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:" + kotlin_version
 
-        /**
-         * Simplifies communication between Activities, Fragments, Threads, Services, etc
-         */
-        val eventbus = "org.greenrobot:eventbus:3.0.0"
+    /**
+     * Simplifies communication between Activities, Fragments, Threads, Services, etc
+     */
+    val eventbus = "org.greenrobot:eventbus:3.0.0"
 
-        val greendao = "org.greenrobot:greendao:3.2.2"
+    val greendao = "org.greenrobot:greendao:3.2.2"
 
-        val guava_android = "com.google.guava:guava:24.0-android"
+    val guava_android = "com.google.guava:guava:24.0-android"
 
-        val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
-        val rxandroid = "io.reactivex.rxjava2:rxandroid:2.0.1"
-        val rxrelay = "com.jakewharton.rxrelay2:rxrelay:2.0.0"
+    val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
+    val rxandroid = "io.reactivex.rxjava2:rxandroid:2.0.1"
+    val rxrelay = "com.jakewharton.rxrelay2:rxrelay:2.0.0"
+
+
+    val dfqin = "com.github.dfqin:grantor:2.5"
+    val appcompat = "androidx.appcompat:appcompat:1.0.0"
+    val core_ktx = "androidx.core:core-ktx:1.0.2"
+    val kotlin_lib = "org.jetbrains.kotlin:kotlin-stdlib:1.7.20"
+
 }
